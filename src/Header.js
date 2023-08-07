659cbf32771867897919c2b2b2e91a494c225d09
leila seif <120723092+Leilaseif@users.noreply.github.com>
2023-08-07 19:07:26 +0330

    first step

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <div className="header">
+       <img className="header-logo" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/092/504/original/icon.png?1691419081" />
+       <p className="header-text">lili contact </p>
+        </div>
+        )    }
